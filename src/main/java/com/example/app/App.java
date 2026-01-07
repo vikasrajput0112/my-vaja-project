930bdb261dbba65db3fa75ad4190f85df49e529f
vikasrajput0112 <devops1@paisalo.in>
2026-01-07 17:42:02 +0530

    Create App.java

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,8 @@
+package com.example.app;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("✅ my-vaja-project built and running successfully!");
+    }
+}
