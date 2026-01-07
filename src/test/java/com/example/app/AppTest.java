a62b48a18303682ad1a978aa2d7e90f894e215d1
vikasrajput0112 <devops1@paisalo.in>
2026-01-07 17:42:49 +0530

    Create AppTest.java

---
+++ src/test/java/com/example/app/AppTest.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void basicSanityTest() {
+        assertTrue(true);
+    }
+}
