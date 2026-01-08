12f900c6b1e8a133bfed67e805e7fafcb86e5ac2
vikasrajput0112 <devops1@paisalo.in>
2026-01-08 12:06:07 +0530

    Update App.java

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,8 +1,11 @@
+
 package com.example.app;
 
 public class App {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("✅ my-vaja-project built and running successfully!");
+
+        // Keep application running
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
